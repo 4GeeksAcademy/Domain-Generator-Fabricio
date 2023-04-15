e0980f4160ad0a2a2e3803165047c5c546f70292
Fabribega <108010149+Fabribega@users.noreply.github.com>
2023-04-14 20:43:37 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,3 +9,25 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+//write your code here
+let dominio = [".com", ".org", ".net", ".io"];
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < noun.length; j++) {
+    for (let p = 0; p < adj.length; p++) {
+      for (let r = 0; r < dominio.length; r++) {
+        console.log(`${pronoun[i]}${noun[j]}${adj[p]}${dominio[r]}`);
+      }
+    }
+  }
+}
